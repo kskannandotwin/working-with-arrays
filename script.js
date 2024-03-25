692e010ff8f447cc6c6742406f8421be17817cda
kannanATexcelforte <kannan@excelforte.com>
2024-03-25 16:26:15 +0530

    25.03.2024-tnam
    completed

--- script.js
+++ script.js
@@ -75,34 +75,14 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
 
-// slice
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
-console.log(arr.slice());
-console.log([...arr]);
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
 
-// splice
-// console.log(arr.splice(2));
-arr.splice(-1);
-console.log(arr);
-arr.splice(1, 2);
-console.log(arr);
-
-// reverse
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
-
-// concate
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
-
-// join
-console.log(letters.join(' - '));
+console.log('Jonas'.at(0));
+console.log('Jonas'.at(-1));
