a66c34c73e2cff14da0952f485750feacd5fdb17
kskannandotwin <kskannan.win@gmail.com>
2024-03-26 05:03:48 +0530

    26.03.2024-pbcde
    completed

--- script.js
+++ script.js
@@ -61,38 +61,22 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  // .textContent = 0;
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>      
+      <div class="movements__value">${mov}€</div>
+    </div>
+    `;
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
 
-/////////////////////////////////////////////////
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-currencies.forEach(function (value, key, map) {
-  console.log(`${key}: ${value}`);
-});
-
-// set
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
-console.log(currenciesUnique);
-currenciesUnique.forEach(function (value, key, map) {
-  console.log(`${key}: ${value}`);
-});
-
-// another type of set
-currenciesUnique.forEach(function (value, _, map) {
-  console.log(`${value}: ${value}`);
-});
+displayMovements(account1.movements);
+// console.log(containerMovements.innerHTML);
