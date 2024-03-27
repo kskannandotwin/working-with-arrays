bc54ff17cc04cb40dade6f2a1ac694e92697574e
kskannandotwin <kskannan.win@gmail.com>
2024-03-27 21:13:42 +0530

    27.03.2024-trm
    completed

--- script.js
+++ script.js
@@ -71,8 +71,9 @@ const displayMovements = function (movements) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
-      <div class="movements__type movements__type--${type}">${i + 1
-      } ${type}</div>      
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>      
       <div class="movements__value">${mov}€</div>
     </div>
     `;
@@ -83,6 +84,15 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+calcDisplayBalance(account1.movements);
+
+console.log(movements);
+
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -91,24 +101,27 @@ const createUsernames = function (accs) {
       .map(name => name[0])
       .join('');
   });
-}
+};
 
 createUsernames(accounts);
-console.log(accounts);
 
-const deposits = movements.filter(function (mov) {
-  return mov > 0;
-});
-console.log(movements);
-console.log(deposits);
+// accumulator -> SNOWBALL
+// const balance = movements.reduce(function(acc, curr, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc += curr;
+// }, 0);
 
-const depositsFor = [];
-for (const mov of movements) if (mov > 0) depositsFor.push(mov);
-console.log(depositsFor);
+const balance = movements.reduce((acc, curr) => (acc += curr), 0);
 
-const withdrawals = movements.filter(mov => mov < 0);
-console.log(withdrawals);
+console.log(balance);
 
-const withdrawalFor = [];
-for (const mov of movements) if (mov < 0) withdrawalFor.push(mov);
-console.log(withdrawalFor);
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// maximum value
+const max = movements.reduce(
+  (acc, mov) => (acc > mov ? acc : mov),
+  movements[0]
+);
+console.log(max);
